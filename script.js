c0ad301805b32414f8f89f900053ea69c0ac66ea
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-23 16:38:34 -0400

    Duplicated confirmation prompts for each character variable

--- script.js
+++ script.js
@@ -4,22 +4,34 @@ var generateBtn = document.querySelector("#generate");
 // Instructor Provided Template: Anthony Cooper
 function generatePassword() {
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = lowercase.toUpperCase();
   var numbers = "0123456789";
   var special = "#$%&()*+,-./:;<=>?";
-  var uppercase = lowercase.toUpperCase();
-
+  
   var input = parseInt(prompt("How many characters will be in your password?"));
   console.log(input);
 
   if (input >= 8 && input <= 128) {
     // Code in this portion of the template is my contribution.
-    var promptLower= true;
+    var promptLower = true;
     var promptLower = confirm("Will your password include lowercase letters?");
     console.log(promptLower);
 
+    var promptUpper = true;
+    var promptUpper = confirm ("Will your password include uppercase letters?");
+    console.log(promptUpper);
+
+    var promptNum = true;
+    var promptNum = confirm ("Will your password include numbers?");
+    console.log(promptNum);
+
+    var promptSpecial = true;
+    var promptSpecial = confirm ("Will your password include special characters?");
+    console.log(promptSpecial);
+
   } else {
-    alert("Password length must between 8 and 128 characters.");
-    writePassword();
+    alert("Password length must a number between 8 and 128 characters.");
+    return writePassword();
   }
 
   return "";
