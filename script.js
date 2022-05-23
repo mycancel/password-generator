fa217b9a83be77feebd3e279f23bc9dbad5e0701
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-23 16:05:52 -0400

    Inserted Instructor Provided Template into JS; deleted onclick event in HTML

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Instructor Provided Template: Anthony Cooper
+function generatePassword() {
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var special = "#$%&()*+,-./:;<=>?";
+  var uppercase = lowercase.toUpperCase();
+
+  var input = parseInt(prompt("Choose a lenght between 8 and 128"));
+
+  if (input >= 8 && input <= 128) {
+    // Code in this portion of the template is my contribution.
+  } else {
+    alert("NOT COOL");
+  }
+
+  return "";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
