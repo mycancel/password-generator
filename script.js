77c4c460b5ca3c7424aa56e4daf0c28d6cfcc55f
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-05-23 15:59:04 -0400

    Started over, removed everything I added since the initial commit

--- script.js
+++ script.js
@@ -13,9 +13,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Opens the first popup. Code inspired by Easy Tutorials Youtube video: https://youtu.be/AF6vGYIyV8M.
-function openCharacter() {
-  characterPopup.classList.add("popup-open");
-  console.log("Opened characterPopup");
-  return;
-}
+
